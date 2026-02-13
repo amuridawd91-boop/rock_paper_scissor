977a296fb29943575e15a0940ce359899ce82ed4
Amuri Dawud <250103254+amuridawd91-boop@users.noreply.github.com>
2026-02-13 15:30:57 +0200

    java.js

--- java.js
+++ java.js
@@ -16,3 +16,21 @@ function getHumanChoice(){
     return humanChoice;
 }
 
+let humanScore = 0
+ let computerScore= 0 
+
+
+function playRound(humanChoice, computerChoice){
+humanChoice = humanChoice.toLowerCase();  
+
+if (humanChoice === computerChoice) {
+    console.log("It's a tie.");
+} else if (humanChoice === "scissor" && computerChoice==="paper" || humanChoice==="rock" && computerChoice==="scissor" || humanChoice==="paper" && computerChoice==="rock"){
+    console.log("You win! "+ humanChoice + " beats " + computerChoice + ".");
+    humanScore++;
+} else {
+    console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
+    computerScore++;
+}  
+
+}
