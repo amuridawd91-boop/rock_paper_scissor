c2753deb8400503ba802722d59fdf04979c5799c
I AM BATMAN <250103254+amuridawd91-boop@users.noreply.github.com>
2026-06-11 03:34:25 +0300

    added play again button

--- java1.js
+++ java1.js
@@ -6,6 +6,8 @@ document.body.appendChild(gameContainer);
 const buttonContainer = document.createElement('div');
 buttonContainer.id = 'buttons';
 gameContainer.appendChild(buttonContainer);
+buttonContainer.style.display = "flex";
+buttonContainer.style.gap =  "10px";
 
 const rockButton = document.createElement('button');
 rockButton.id = 'rock';
@@ -22,6 +24,11 @@ scissorButton.id = 'scissor';
 scissorButton.textContent = 'Scissor';
 buttonContainer.appendChild(scissorButton);
 
+const playAgain = document.createElement("button");
+playAgain.id = "play-again";
+playAgain.textContent = "Play Again";
+playAgain.style.display = "none";
+buttonContainer.appendChild(playAgain);
 
 const resultsContainer = document.createElement('div');
 resultsContainer.id = 'results-container';
@@ -42,6 +49,8 @@ finalWinner.id = 'final-winner';
 finalWinner.style.color = 'darkbrown';
 resultsContainer.appendChild(finalWinner);
  
+let humanScore = 0
+let computerScore= 0 
 
 function getComputerChoice(){
     let random = Math.floor(Math.random()*3);
@@ -55,16 +64,14 @@ function getComputerChoice(){
 
 }
 
-function getHumanChoice(){
-    let humanChoice = prompt("Enter rock, paper or scissor", "");
-    return humanChoice;
-}
-
-let humanScore = 0
- let computerScore= 0 
+// function getHumanChoice(){
+//     let humanChoice = prompt("Enter rock, paper or scissor", "");
+//     return humanChoice;
+// }
 
 
 function playRound(humanChoice, computerChoice){
+if (humanScore === 5 || computerScore === 5) return;
 humanChoice = humanChoice.toLowerCase();  
 
 if (humanChoice === computerChoice) {
@@ -75,42 +82,63 @@ if (humanChoice === computerChoice) {
     roundLog.textContent = "You lose! " + computerChoice + " beats " + humanChoice + ".";
     computerScore++;
 }  
-
+    ultimateWinner();
 }
 
-function playGame(){
-    humanScore = 0;
-    computerScore = 0;
-    for (i=1;i<=5;i++){
-        console.log("Round " + i);
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection)
-    console.log("Human Score;" + humanScore, "Computer Score; " + computerScore );
-    }
-    if (humanScore > computerScore){
-        console.log("Congratulations you won the game!!");
-    } else {
-        console.log("Sorry you lost the game.")
-    }
-}
-playGame()
+// function playGame(){
+//     humanScore = 0;
+//     computerScore = 0;
+//     for (i=1;i<=5;i++){
+//         console.log("Round " + i);
+//     const humanSelection = getHumanChoice();
+//     const computerSelection = getComputerChoice();
+//         playRound(humanSelection, computerSelection)
+//     console.log("Human Score;" + humanScore, "Computer Score; " + computerScore );
+//     }
+//     if (humanScore > computerScore){
+//         console.log("Congratulations you won the game!!");
+//     } else {
+//         console.log("Sorry you lost the game.")
+//     }
+// }
+// playGame()
 
 function ultimateWinner() {
     scoreBoard.textContent = "Human Score: " + humanScore + "|  Computer Score: " + computerScore;
     if (humanScore == 5){
         finalWinner.textContent = "Congratulations you are the WINNER!"
+        disableButtons();
+        playAgain.style.display = "block";
     } else if (computerScore == 5){
         finalWinner.textContent = "Sorry you lost the game. Try Again!"
+        disableButtons();
+        playAgain.style.display = "block";
     }
 }
 
+function disableButtons(){
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorButton.disabled = true;
+}
+
+playAgain.addEventListener("click", () =>{
+    humanScore = 0;
+    computerScore = 0;
+    roundLog.textContent = "Click a button to start the game!";
+    scoreBoard.textContent = 'Human Score: 0 | Computer Score: 0';
+    finalWinner.textContent = "";
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorButton.disabled = false;
+    playAgain.style.display = "none";
+})
 rockButton.addEventListener("click", () => {
-    playRound("rock", getComputerChoice)
+    playRound("rock", getComputerChoice())
 });
 paperButton.addEventListener("click", () => {
-    playRound("paper", getComputerChoice)
+    playRound("paper", getComputerChoice())
 });
 scissorButton.addEventListener("click", () => {
-    playRound("scissor", getComputerChoice)
+    playRound("scissor", getComputerChoice())
 });
